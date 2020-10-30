dae32953c518be97943c03133ada9d69737b9f4f
IUGLYI <73348378+IUGLYI@users.noreply.github.com>
2020-10-30 22:11:48 +0300

    Update and rename CAT.py to Class.py

--- Class.py
+++ Class.py
@@ -10,10 +10,3 @@ class Cat():
     def description_name(self):
         desc = self.name + ' ' + self.gender + ' ' + str(self.age) + ' ' + self.breed
         return desc.title()
-
-
-cat1 = Cat('Baron', 'Boy', 2, 'Siberian')
-cat2 = Cat('Sam', 'Boy', 2, 'Siberian')
-
-print(cat1.description_name())
-print(cat2.description_name())
