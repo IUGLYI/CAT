bea077f7499594c85125b8aa571911e87936c68c
IUGLYI <73348378+IUGLYI@users.noreply.github.com>
2020-10-30 22:50:33 +0300

    Update Class.py

--- Class.py
+++ Class.py
@@ -1,15 +1,14 @@
 # CAT
 
-  
 class Cat:
-    def __init__(self, name, gender, age):
+    def __init__(self, name, gender,age):
         self.name = name
         self.gender = gender
         self.age = age
 
-    def getName(self):
+    def get_name(self):
         return self.name
-    def getGender(self):
+    def get_gender(self):
         return self.gender
-    def getAge(self):
+    def get_age(self):
         return self.age
