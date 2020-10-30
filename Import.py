ac46ab13a20f90e9520136d874cbc69b08d94dbe
IUGLYI <73348378+IUGLYI@users.noreply.github.com>
2020-10-30 22:56:39 +0300

    Update Import.py

--- Import.py
+++ Import.py
@@ -5,7 +5,7 @@ cat2 = Cat('Baron', 'Boy', 2)
 
 cats =[cat1, cat2]
 
-for cat in  cats:
+for cat in cats:
     print('Name:', cat.get_name(),
           'Gender:', cat.get_gender(),
           'Age:', cat.get_age())
